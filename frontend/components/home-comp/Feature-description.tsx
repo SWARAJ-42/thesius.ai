5c75c49503dba60118d8b5ca0ddbe3a680ebc94f
SWARAJ-42 <biswalswaraj88@kgpian.iitkgp.ac.in>
2024-12-13 09:29:40 +0530

    Landing page tweaks

--- frontend/components/home-comp/Feature-description.tsx
+++ frontend/components/home-comp/Feature-description.tsx
@@ -4,9 +4,12 @@ import "./custom-styles/2-feature-description.css";
 import React, { useRef } from "react";
 import useVisibility from "@/lib/visibility-detector";
 import Image, { StaticImageData } from "next/image";
-import newsimg from "@/assets/Home/FeatureDescription/news.png";
-import headstartimg from "@/assets/Home/FeatureDescription/headstart.png";
-import researchimg from "@/assets/Home/FeatureDescription/research.png";
+import projectplannerimg from "@/assets/Home/FeatureDescription/project-planner.png";
+import searchresultimg from "@/assets/Home/FeatureDescription/search-results.png";
+import multiresultchatimg from "@/assets/Home/FeatureDescription/multi-result-chat.png";
+import paperdetailsimg from "@/assets/Home/FeatureDescription/paper-details.png";
+import paperchatimg from "@/assets/Home/FeatureDescription/paper-chat.png";
+import { useRouter } from "next/navigation";
 
 interface Feature {
   image: {
@@ -21,10 +24,11 @@ interface Feature {
 }
 
 export function FeatureDescription() {
+  const router = useRouter()
   const featuresData: Feature[] = [
     {
       image: {
-        src: headstartimg,
+        src: projectplannerimg,
         alt: "Feature Image 1",
       },
       design: "md:flex-row",
@@ -36,7 +40,7 @@ export function FeatureDescription() {
     },
     {
       image: {
-        src: headstartimg,
+        src: searchresultimg,
         alt: "Feature Image 1",
       },
       design: "md:flex-row-reverse",
@@ -48,7 +52,7 @@ export function FeatureDescription() {
     },
     {
       image: {
-        src: newsimg,
+        src: multiresultchatimg,
         alt: "Feature Image 2",
       },
       design: "md:flex-row",
@@ -59,14 +63,25 @@ export function FeatureDescription() {
     },
     {
       image: {
-        src: researchimg,
+        src: paperdetailsimg,
         alt: "Feature Image 3",
       },
       design: "md:flex-row-reverse",
       heading: "Paper Exploration",
       description: "Select any research paper to view detailed information such as citations, key findings, and many more. Engage directly with the paper, ask questions, and explore additional insights to support your work",
       buttonText: "Get Started",
-      animation: ["slide-in-left", "slide-out-eight"],
+      animation: ["slide-in-left", "slide-out-right"],
+    },
+    {
+      image: {
+        src: paperchatimg,
+        alt: "Feature Image 4",
+      },
+      design: "md:flex-row",
+      heading: "Research Paper Dissection",
+      description: "Engage with research papers through LLM-powered chats, offering section-wise breakdowns, contextual explanations, and tailored discussions for efficient understanding and knowledge extraction.",
+      buttonText: "Get Started",
+      animation: ["slide-in-right", "slide-out-left"],
     },
   ];
 
@@ -82,7 +97,8 @@ export function FeatureDescription() {
             <div
               ref={ref}
               key={index}
-              className={`feature-animation ${isVisible ? feature.animation[0] : feature.animation[1]} flex flex-col ${feature.design} px-10 justify-between items-center mx-auto max-w-6xl min-h-[300px] rounded-2xl overflow-hidden bg-gradient-to-r backdrop-blur-sm from-green-500/30 to-green-200/30 shadow-lg`}
+              // feature-animation ${isVisible ? feature.animation[0] : feature.animation[1]} 
+              className={`flex flex-col ${feature.design} px-10 justify-between items-center mx-auto max-w-6xl min-h-[300px] rounded-2xl overflow-hidden bg-gradient-to-r backdrop-blur-sm from-green-500/30 to-green-200/30 shadow-lg`}
             >
               <div className="flex-1 relative w-full hidden sm:block px-4">
                 <Image
@@ -98,7 +114,7 @@ export function FeatureDescription() {
                 <p className="text-md md:text-xl text-[#3b3b3b]/90 mb-6">
                   {feature.description}
                 </p>
-                <button className="bg-white text-xl text-green-600 px-8 py-4 rounded-xl font-semibold hover:bg-opacity-90 transition-colors duration-300">
+                <button onClick={() => {router.push("/auth/login")}} className="bg-white text-xl text-green-600 px-8 py-4 rounded-xl font-semibold hover:bg-opacity-60 transition-colors duration-300">
                   {feature.buttonText}
                 </button>
               </div>
