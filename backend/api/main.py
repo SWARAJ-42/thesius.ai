ced04c3d1e694c4c603811ea7fbf3201d6b67760
SWARAJ-42 <biswalswaraj88@kgpian.iitkgp.ac.in>
2024-12-16 18:46:09 +0530

    CORS resolving and deployment changes

--- backend/api/main.py
+++ backend/api/main.py
@@ -2,22 +2,25 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.routers import auth, search_engine, paper_details, contact
 from api.database import Base, engine
+from dotenv import load_env
+import os
+
+load_env()
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
+allowed_origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost",
-        "http://<YOUR_CUSTOM_DNS>",
-        "http://<YOUR_CUSTOM_IP>"
-    ],  # Add custom DNS and IP
+    allow_origins=allowed_origins,
     allow_credentials=True,
-    allow_methods=["*"],  # Restrict if needed
-    allow_headers=["*"],  # Restrict if needed
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 @app.get("/")
 def health_check():
--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -1,1 +1,5 @@
-export const BACKEND_URL="http://localhost:8000"
+const isProd = process.env.NODE_ENV === "production";
+
+export const BACKEND_URL = isProd 
+    ? "http://backend:8000"  // Internal Docker URL for production
+    : "http://localhost:8000";  // Localhost for development
