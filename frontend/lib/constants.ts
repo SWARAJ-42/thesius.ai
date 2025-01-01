e534b926a809f4ce700b812d74cf7a46faadc9fd
SWARAJ-42 <biswalswaraj88@kgpian.iitkgp.ac.in>
2025-01-01 11:35:07 +0530

    nginx basic setup done

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -1,4 +1,4 @@
 const isProd = process.env.NEXTJS_ENV === "production";
 
-export const BACKEND_URL = "http://thesius-ai.centralindia.cloudapp.azure.com:8000"
-// export const BACKEND_URL = "http://localhost:8000"
+// export const BACKEND_URL = "http://thesius-ai.centralindia.cloudapp.azure.com:8000"
+export const BACKEND_URL = "http://localhost/server"
